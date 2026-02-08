ae9ec4b2c253805db5cd0a7a2c2d8ab4d18ac938
Prashant Shrestha <prashantshrestha700@gmail.com>
2026-02-09 01:17:14 +0530

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@website-convex/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
